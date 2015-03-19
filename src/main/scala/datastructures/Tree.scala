603bb5014a54d65b0e0e2d96ae632d94248898cf
benivf <beni.villa@gmail.com>
2015-03-19 13:11:05 +0000

    Refactor the name of the variable and method.

--- src/main/scala/datastructures/Tree.scala
+++ src/main/scala/datastructures/Tree.scala
@@ -16,21 +16,21 @@ object Tree {
   }
 
   def maximum(tree: Tree[Int]): Option[Int] = {
-    def maxValue(first: Option[Int], second: Option[Int]): Option[Int] = (first, second) match {
+    def max(first: Option[Int], second: Option[Int]): Option[Int] = (first, second) match {
       case (Some(firstValue), Some(secondValue)) if firstValue >= secondValue => first
       case (Some(firstValue), Some(secondValue)) if secondValue > firstValue => second
       case (Some(_), None) => first
       case (None, Some(_)) => second
     }
-    def go(tree: Tree[Int], max: Option[Int] ): Option[Int] = (tree, max) match {
+    def go(tree: Tree[Int], currentMax: Option[Int] ): Option[Int] = (tree, currentMax) match {
       case (Leaf(value), None) => Some(value)
       case (Leaf(value), Some(maxValue)) if value>maxValue  => Some(value)
       case (Branch(left, right), maxOption) => {
         val maxLeft = maximum(left)
         val maxRight = maximum(right)
-        maxValue(maxValue(maxLeft, maxRight), max)
+        max(max(maxLeft, maxRight), currentMax)
       }
-      case _ => max
+      case _ => currentMax
     }
     go(tree, None)
   }
