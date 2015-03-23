f26612e335de6ade517b7c41c7a05f70751009f2
benivf <beni.villa@gmail.com>
2015-03-23 13:25:23 +0000

    Add more test for map2.

--- src/test/scala/errorhandling/EitherSpec.scala
+++ src/test/scala/errorhandling/EitherSpec.scala
@@ -1,10 +1,23 @@
 package errorhandling
 
+
 import org.scalatest.{Matchers, FlatSpec}
 import Matchers._
 
 
 class EitherSpec extends FlatSpec {
+
+  case class Person(name: Name, age: Age)
+  sealed class Name(val value: String)
+  sealed class Age(val value: Int)
+  def mkName(name: String): Either[String, Name] =
+    if (name == "" || name == null) Left("Name is empty.")
+    else Right(new Name(name))
+  def mkAge(age: Int): Either[String, Age] =
+    if (age < 0) Left("Age is out of range.")
+    else Right(new Age(age))
+  def mkPerson(name: String, age: Int): Either[String, Person] =
+    mkName(name).map2(mkAge(age))(Person(_, _))
 
   it should "map" in {
     Left("there is a problem").map(_.toString + " add something") shouldBe Left("there is a problem")
@@ -35,6 +48,15 @@ class EitherSpec extends FlatSpec {
   it should "map2" in {
     Right(44).map2(Left(21))(_+_) shouldBe Left(21)
     Right(44).map2(Right(21))(_+_) shouldBe Right(65)
+    mkPerson("", 10) shouldBe Left("Name is empty.")
+    mkPerson("John", -10) shouldBe Left("Age is out of range.")
+    import org.scalatest.Inside._
+    inside(mkPerson("John", 10)) {
+      case Right(Person(name, age)) =>
+        name.value shouldBe "John"
+        age.value shouldBe 10
+    }
   }
 
 }
+
