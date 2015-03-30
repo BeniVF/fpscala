4c4dec9342d4f0ed7f0b33c4c59e8706e8a66cf9
benivf <beni.villa@gmail.com>
2015-03-30 13:42:24 +0100

    Implement intDouble function on RNG.

--- src/test/scala/state/StateSpec.scala
+++ src/test/scala/state/StateSpec.scala
@@ -6,7 +6,6 @@ import Matchers._
 class StateSpec extends FlatSpec {
   import RNG._
 
-
   def generateRNGs(test : RNG => Unit): Unit = {
     for (i <- 0 to 10000) {
       test(Simple(i))
@@ -26,6 +25,16 @@ class StateSpec extends FlatSpec {
       val (value, newRng) = double(rng)
       value should be >= 0.0
       value should be <= 1.0
+      newRng should not be(rng)
+    }
+  }
+
+  it should "generate a random pair of Int and Double" in {
+    generateRNGs { rng =>
+      val ((intValue, doubleValue), newRng) = intDouble(rng)
+      intValue should be >= 0
+      doubleValue should be >= 0.0
+      doubleValue should be <= 1.0
       newRng should not be(rng)
     }
   }
