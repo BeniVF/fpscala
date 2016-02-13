74038181b3ca127690aa87b0b23eec285dc99b0b
benivf <beni.villa@gmail.com>
2016-02-13 11:59:19 +0000

    Implement sorted function

--- src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -151,7 +151,19 @@ object PolymorphicFunctions {
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+    def go(i: Int): Boolean = 
+      if (as.length == 0 || i == as.length - 1) 
+        true
+      else {
+        if (gt(as(i), as(i+1)))
+          go(i+1)
+        else
+          false 
+        }
+    go(0)
+
+  }
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
--- src/test/scala/fpinscala/gettingstarted/GettingStartedSpec.scala
+++ src/test/scala/fpinscala/gettingstarted/GettingStartedSpec.scala
@@ -6,6 +6,7 @@ import org.scalacheck.Gen
 
 object GettingStartedSpec extends Properties("GettingStartedSpec") {
 	import MyModule._
+	import PolymorphicFunctions._
 
 	val positiveNumbers = Gen.posNum[Int]
 
@@ -15,5 +16,21 @@ object GettingStartedSpec extends Properties("GettingStartedSpec") {
 		else
 			fib(a) == fib(a-1) + fib(a-2)
 	}
+	val genIntArray = Gen.containerOf[Array, Int](
+		Gen.chooseNum(Int.MinValue, Int.MaxValue)
+	)
+	val genSortedIntArray = genIntArray.map(_.sorted.zipWithIndex.map{case (value, index) => value+2*index}.sorted)
+
+	property("sorted array") = forAll(genSortedIntArray) { (a: Array[Int]) =>
+		isSorted(a, (a: Int, b: Int) => a<=b) == true
+	}
+
+	property("unsorted array") = forAll(genSortedIntArray) { (c: Array[Int]) =>
+		if (c.length > 2 ) 
+			isSorted(c, (a: Int, b: Int) => a>b || b == a) == false
+		else true
+	}
+
+
 
 }
