98f522cdba14937ac783b5667f4990a6b88dac69
benivf <beni.villa@gmail.com>
2017-03-11 19:44:35 +0000

    Add foldMap

--- src/main/scala/fpinscala/monoid/Monoid.scala
+++ src/main/scala/fpinscala/monoid/Monoid.scala
@@ -3,11 +3,19 @@ package fpinscala.monoid
 
 trait Monoid[A] {
   def op(a1: A, a2: A): A
+
   def zero: A
 }
 
 
 object Monoid {
+
+  val stringAddition: Monoid[String] = new Monoid[String] {
+    def op(a1: String, a2: String) = a1 + a2
+
+    def zero = ""
+  }
+
   val intAddition: Monoid[Int] = new Monoid[Int] {
     def op(a1: Int, a2: Int) = a1 + a2
 
@@ -20,23 +28,22 @@ object Monoid {
     def zero = 1
   }
 
-  val booleanOr: Monoid[Boolean] =  new Monoid[Boolean] {
+  val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
     def op(a1: Boolean, a2: Boolean) = a1 || a2
 
     def zero = false
   }
 
-  val booleanAnd: Monoid[Boolean] =  new Monoid[Boolean] {
+  val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
     def op(a1: Boolean, a2: Boolean) = a1 && a2
 
     def zero = true
   }
 
-
-  def option[A]: Monoid[Option[A]] =  new Monoid[Option[A]] {
+  def option[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
     def op(a1: Option[A], a2: Option[A]): Option[A] = a1 orElse a2
 
-    def zero : Option[A] = None
+    def zero: Option[A] = None
   }
 
   def endo[A]: Monoid[A => A] = new Monoid[(A) => A] {
@@ -45,5 +52,13 @@ object Monoid {
     def zero = identity
   }
 
+  def foldMap[A, B](as: List[A], m: Monoid[B])(f: A => B): B = as.map(f).foldLeft(m.zero)(m.op)
 
+  def foldMapV[A, B](v: IndexedSeq[A], m: Monoid[B])(f: A => B): B = v match {
+    case a if a.isEmpty => m.zero
+    case a if a.size == 1 => f(v.head)
+    case _ =>
+      val (l, r) = v.splitAt(v.size / 2)
+      m.op(foldMapV(l, m)(f), foldMapV(r, m)(f))
+  }
 }
--- src/test/scala/fpinscala/monoid/MonoidSpec.scala
+++ src/test/scala/fpinscala/monoid/MonoidSpec.scala
@@ -1,41 +1,21 @@
 package fpinscala.monoid
 
-import org.scalacheck.Prop.forAll
-import org.scalacheck.{Arbitrary, Gen, Properties}
+import org.scalatest.FlatSpec
 
+import org.scalatest.Matchers._
 
-object MonoidSpec extends Properties("GettingStartedSpec") {
+class MonoidSpec extends FlatSpec {
 
-  implicit val intNumbers  = Arbitrary.arbInt.arbitrary
-  implicit val boolean  = Arbitrary.arbBool.arbitrary
-  implicit val option  = Arbitrary.arbOption[Int].arbitrary
-  implicit val functionIntToInt  = Arbitrary.arbFunction1[Int, Int].arbitrary
-
-
-  def monoidLaws[A](monoid: Monoid[A])(implicit aGen: Gen[A]) = forAll(aGen, aGen, aGen) { (x: A, y: A, z:A ) =>
-    monoid.op(monoid.op(x, y), monoid.zero) == monoid.op(x, monoid.op(y, monoid.zero)) &&
-      monoid.op(monoid.op(x, y), z) == monoid.op(x, monoid.op(y, z))
+  it should "fold map using monoids" in {
+    Monoid.foldMap(List(1,2,3), Monoid.stringAddition)(_.toString) shouldBe "123"
+    Monoid.foldMap(List("5","2","3"), Monoid.intMultiplication)(_.toInt) shouldBe 30
+    Monoid.foldMap(List("5","2","3"), Monoid.intAddition)(_.toInt) shouldBe 10
   }
 
-  def endoMonoidLaws[A](monoid: Monoid[A => A])(implicit aFunGen: Gen[A => A], aGen: Gen[A]) = forAll(aFunGen, aFunGen, aFunGen, aGen) {
-    (x: A => A, y: A => A, z: A => A, a: A ) =>
-    val op = monoid.op(x, y)
-    monoid.op(op, monoid.zero).apply(a) == monoid.op(x, monoid.op(y, monoid.zero)).apply(a) &&
-      monoid.op(monoid.op(x, y), z).apply(a) == monoid.op(x, monoid.op(y, z)).apply(a)
+  it should "fold map v using monoids" in {
+    Monoid.foldMapV(Vector(1,2,3), Monoid.stringAddition)(_.toString) shouldBe "123"
+    Monoid.foldMapV(Vector("5","2","3"), Monoid.intMultiplication)(_.toInt) shouldBe 30
+    Monoid.foldMapV(Vector("5","2","3"), Monoid.intAddition)(_.toInt) shouldBe 10
   }
-
-  property("int addition monoids") = monoidLaws(Monoid.intAddition)
-
-  property("int multiplication monoids") = monoidLaws(Monoid.intMultiplication)
-
-  property("boolean or monoids") = monoidLaws(Monoid.booleanOr)
-
-  property("boolean and monoids") = monoidLaws(Monoid.booleanAnd)
-
-  property("option of int monoids") = monoidLaws(Monoid.option[Int])
-
-  property("endfunction of int monoids") = endoMonoidLaws(Monoid.endo[Int])
 
 }
-
-
